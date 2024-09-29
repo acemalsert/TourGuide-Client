66c6b91b0bd3e18bd0be0cc35fc98599a1883347
AhmetCemalSert <146725922+AhmetCemalSert@users.noreply.github.com>
2024-09-29 11:46:15 +0300

    search bar is added to destinations page

--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; 
-import "./destinations.css"
+import { useNavigate } from 'react-router-dom';
+import "./destinations.css";
 
 const Destinations = () => {
   const [destinations, setDestinations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
   const fetchDestinations = async () => {
@@ -14,16 +15,15 @@ const Destinations = () => {
       const res = await axios.get(`http://localhost:5008/api/Destination/GetAllDestinations`);
       setDestinations(res.data);
       setLoading(false);
-      console.log(res);
     } catch (error) {
-      setError(error.message); 
-      setLoading(false); 
+      setError(error.message);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchDestinations();
-  }, []); // Component mount olduğunda bir kez çalışır
+  }, []);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -32,20 +32,34 @@ const Destinations = () => {
     navigate(`/destinations/${id}`);
   };
 
-  //TODO : Bir arama cubugu eklenmeli
+  const filteredDestinations = destinations.filter((destination) =>
+    destination.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    destination.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <div className="destinations">
-      <h2>Destinations</h2>
-      <div className="row">
-        {destinations.map((destination) => (
+    <div className="destinations justify-content-center mb-4">
+
+      <div className='input-group justify-content-center'>
+        <div className="form-outline" data-mdb-input-init>
+          <input
+            type="search"
+            id="form1"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input form-control"
+          />
+        </div>
+      </div>
+
+      <div className="row mt-4">
+        {filteredDestinations.map((destination) => (
           <div className="card" style={{ width: '18rem', margin: '10px' }} key={destination.id}>
-            <img className="card-img-top" src="https://www.bizevdeyokuz.com/wp-content/uploads/agra-tac-mahal.jpg" alt="Destination Image" /> {/* Burada destination'a ait bir image url eklemelisin */}
+            <img className="card-img-top" src="https://www.bizevdeyokuz.com/wp-content/uploads/agra-tac-mahal.jpg" alt="Destination" />
             <div className="card-body">
               <h5 className="card-title">{destination.name}</h5>
-              <p className="card-text">
-                {destination.description}
-              </p>
+              <p className="card-text">{destination.description}</p>
               <button className="btn btn-primary" onClick={() => goToDetails(destination.id)}>
                 Go to Details
               </button>
