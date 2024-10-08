ccfdae105d60e79f907a71b4d5ceca9b916e24c8
AhmetCemalSert <146725922+AhmetCemalSert@users.noreply.github.com>
2024-10-08 16:43:59 +0300

    Template for register page is created

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  
   const [email, setEmail] = useState('');
 
   const handleSubmit = (e) => {
@@ -13,37 +15,97 @@ const Register = () => {
 
   return (
     <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+      <section className="vh-100" style={{ backgroundColor: '#eee' }}>
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black" style={{ borderRadius: '25px' }}>
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                      <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input 
+                              type="text" 
+                              id="form3Example1c" 
+                              className="form-control"
+                              value={username}
+                              onChange={(e) => setUsername(e.target.value)} 
+                            />
+                            <label className="form-label" htmlFor="form3Example1c">Your Name</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input 
+                              type="email" 
+                              id="form3Example3c" 
+                              className="form-control"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <label className="form-label" htmlFor="form3Example3c">Your Email</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input 
+                              type="password" 
+                              id="form3Example4c" 
+                              className="form-control"
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <label className="form-label" htmlFor="form3Example4c">Password</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input 
+                              type="password-repeat" 
+                              id="form3Example4c2" 
+                              className="form-control"
+                              value={repeatPassword}
+                              onChange={(e) => setRepeatPassword(e.target.value)}
+                            />
+                            <label className="form-label" htmlFor="form3Example4c">Repeat Password</label>
+                          </div>
+                        </div>
+
+                        <div className="form-check d-flex justify-content-center mb-5">
+                          <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                          <label className="form-check-label" htmlFor="form2Example3">
+                            I agree to all statements in <a href="#!">Terms of Service</a>
+                          </label>
+                        </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="submit" className="btn btn-primary btn-lg">Register</button>
+                        </div>
+                      </form>
+                    </div>
+
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                        className="img-fluid" alt="Sample image" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      </section>
     </div>
   );
 };
