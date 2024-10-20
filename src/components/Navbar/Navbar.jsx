170254b652be2982726c3d2816e1f83fd3003820
AhmetCemalSert <146725922+AhmetCemalSert@users.noreply.github.com>
2024-10-20 22:42:35 +0300

    profile page link is added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import './navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('expiration');
+    navigate('/login'); 
+  };
+
+  const isLoggedIn = !!localStorage.getItem('token'); 
+
   return (
     <div className='container'>
-    <div className='navbar'>
-      <nav>
-      <ul>
-        <li><Link to="/destinations">Destinations</Link></li>
-        <li><Link to="/guides">Guides</Link></li>
-        <li><Link to="/">Map</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-      </ul>
-      </nav>
-    </div>
+      <div className='navbar'>
+        <nav>
+          <ul>
+            <li><Link to="/destinations">Destinations</Link></li>
+            <li><Link to="/guides">Guides</Link></li>
+            <li><Link to="/">Map</Link></li>
+            <li><Link to="/about">About</Link></li>
+
+            {isLoggedIn ? (
+              <>
+                <li>
+                  <Link to="/profile"> 
+                    <img 
+                      src="../src/assets/default_profile_photo.webp" 
+                      alt="Profile"
+                      className="profile-photo"
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <button onClick={handleSignOut} className="btn btn-danger">
+                    Sign Out
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
